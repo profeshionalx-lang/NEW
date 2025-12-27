12553732afaf550c5e447fd7a84cf1f0f9919e0b
profeshionalx-lang <profeshionalx@gmail.com>
2025-12-27 15:28:58 +0100

    Add Firebase configuration and initialization code

---
+++ config.js
@@ -0,0 +1,33 @@
+// js/config.js
+const CONFIG = {
+    ADMIN_EMAIL: 'profeshionalx@gmail.com',
+    FIREBASE: {
+        apiKey: "AIzaSyBVKxWXOxVq_9vIpRr7eQCEJOPv7PKImg0",
+        authDomain: "grechka-6bdb7.firebaseapp.com",
+        projectId: "grechka-6bdb7",
+        storageBucket: "grechka-6bdb7.firebasestorage.app",
+        messagingSenderId: "746199017331",
+        appId: "1:746199017331:web:2e4e08023cb89484fd4706"
+    }
+};
+
+// Firebase Init
+(function() {
+    if (typeof firebase === 'undefined' || firebase.apps.length) return;
+    
+    firebase.initializeApp(CONFIG.FIREBASE);
+    const db = firebase.firestore();
+    const auth = firebase.auth();
+    
+    window.fb = {
+        db, auth,
+        googleProvider: new firebase.auth.GoogleAuthProvider(),
+        collection: (name) => db.collection(name),
+        doc: (col, id) => db.collection(col).doc(id),
+        increment: firebase.firestore.FieldValue.increment,
+        arrayUnion: firebase.firestore.FieldValue.arrayUnion,
+        arrayRemove: firebase.firestore.FieldValue.arrayRemove
+    };
+    
+    window.dispatchEvent(new Event('firebaseReady'));
+})();
